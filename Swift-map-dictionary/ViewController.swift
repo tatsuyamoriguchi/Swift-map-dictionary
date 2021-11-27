da7f2ac6acff1a88b96fc7fa9b321db4962aef53
Brian Moriguchi <brian@beckos.com>
2021-11-26 19:07:58 -0800

    Return keys of dictionary Sort key array

--- Swift-map-dictionary/ViewController.swift
+++ Swift-map-dictionary/ViewController.swift
@@ -11,7 +11,16 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let months = [1:"January", 2:"February", 3:"March", 4:"April", 5:"May", 6:"June", 7:"July", 8:"August", 9:"September", 10:"October", 11:"November", 12:"December"]
+
+        let monthDictionaryKeys = months.map { (key, value) -> Int in
+            return key
+        }
+        
+        print(monthDictionaryKeys) // [3, 12, 4, 1, 5, 9, 6, 10, 8, 2, 7, 11]
+        
+        let sortedMonths = monthDictionaryKeys.sorted { $0 < $1 }
+        print(sortedMonths) // [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
     }
 
 
